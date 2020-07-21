38adc2413536cc113b63ce792df3f3e2e8b3537d
Thomas Kottke <t.kottke90@gmail.com>
2020-07-20 20:45:47 -0500

    feat: Create user route

---
+++ project/functions/src/routes/users.route.ts
@@ -0,0 +1,48 @@
+import FirestoreClass from "../classes/firestore.class";
+import Application from "../classes/application.class";
+
+import UserModel from '../models/users.model';
+import { IHooksArray } from "../interfaces/routing.interfaces";
+
+class UserRoute extends FirestoreClass {
+
+  private beforeHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
+
+  private afterHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
+
+  private errorHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
+
+  constructor(app: Application) {
+    super(app, 'users');
+
+    this.configure('users', UserModel, { before: this.beforeHooks, after: this.afterHooks, error: this.errorHooks });
+  }
+}
+
+exports.initialize = (app: Application) => {
+  return new UserRoute(app);
+}
