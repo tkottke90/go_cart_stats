f93d24c9e74d6b376188dd4df4e98c91db6be1ef
Thomas Kottke <t.kottke90@gmail.com>
2020-07-21 06:59:02 -0500

    feat: Add races route

---
+++ project/functions/src/routes/races.route.ts
@@ -0,0 +1,48 @@
+import FirestoreClass from "../classes/firestore.class";
+import Application from "../classes/application.class";
+
+import RaceModel from '../models/race.model';
+import { IHooksArray } from "../interfaces/routing.interfaces";
+
+class RaceRoute extends FirestoreClass {
+
+  private beforeHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
+
+  private afterHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
+
+  private errorHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
+
+  constructor(app: Application) {
+    super(app, 'races');
+
+    this.configure('races', RaceModel, { before: this.beforeHooks, after: this.afterHooks, error: this.errorHooks });
+  }
+}
+
+exports.initialize = (app: Application) => {
+  return new RaceRoute(app);
+}
