7f7cd7006070bf3b038dd5c17a5707c205476b1a
Thomas Kottke <t.kottke90@gmail.com>
2020-07-20 20:45:29 -0500

    refactor: Add userId to user obj

--- project/functions/src/models/users.model.ts
+++ project/functions/src/models/users.model.ts
@@ -1,6 +1,7 @@
 import * as Joi from '@hapi/joi';
 
 export default Joi.object({
+    userId: Joi.string().allow(''),
     experience: Joi.number().positive(),
     rival: Joi.string().allow(''),
     number: Joi.number().positive()
