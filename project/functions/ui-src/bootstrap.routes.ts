360021580833e04a1ae7b90623d2c425ce063a71
Thomas Kottke <t.kottke90@gmail.com>
2020-07-22 21:28:25 -0500

    feat: Add auth route guard to home

--- project/functions/ui-src/bootstrap.routes.ts
+++ project/functions/ui-src/bootstrap.routes.ts
@@ -1,5 +1,8 @@
 // import { redirectInvalidAuthentication } from './app/utilities/authentication-helpers';
 import { Route } from './app/util/route';
+import { Router } from './app/router';
+
+import authGuard from './app/guards/auth.guard';
 
 const routes: Route[] = [
   new Route({
@@ -17,8 +20,17 @@ const routes: Route[] = [
     promise: () => import('./app/pages/home/home'),
     render: true,
     tag: 'home-component',
-    before: (route, context, next) => {
+    before: async (route, context, next) => {
+      const allowed = authGuard();
       
+      console.dir(allowed);
+
+      if (!allowed) {
+        Router.navigate('/login');
+        return;
+      }
+
+      next();
     }
   })
 ];
