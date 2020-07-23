6901bda61ce2ace9723ebb56744a99f9ffb8f3d0
Thomas Kottke <t.kottke90@gmail.com>
2020-07-22 21:33:43 -0500

    refactor(firebase-service): Add auth fn

--- project/functions/ui-src/app/services/firebase.service.ts
+++ project/functions/ui-src/app/services/firebase.service.ts
@@ -1,32 +1,33 @@
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
+
+fetch('/__/firebase/init.json').then(async response => {
+  firebase.initializeApp(await response.json());
+
+  // As httpOnly cookies are to be used, do not persist any state client side.
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+});
 
 import 'firebase/auth';
-import 'firebase/analytics'
 
 export default class FirebaseService {
 
-  constructor() {
-    firebase.initializeApp({});
+  public static currentUser(): firebase.User | null {
+    return firebase.auth().currentUser;
   }
 
-  public static signInWithUNPW(email: string, password: string) {
+  public static signInWithUNPW(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    console.log(email, password);
+
     return firebase.auth().signInWithEmailAndPassword(email, password);
   }
 
-  public static async signInWIthGoogle() {
+  public static signInWIthGoogle(): Promise<firebase.auth.UserCredential> {
     const provider = new firebase.auth.GoogleAuthProvider();
-    
-    try {
-      const loginAttempt = firebase.auth().signInWithPopup(provider);
-    
-    } catch (err) {
-      console.error(err);
-      firebase.analytics().logEvent('exception', { error: err });
-      return false;
-    }
+
+    return firebase.auth().signInWithPopup(provider);
   }
 
-  public static signOut() {
+  public static signOut(): Promise<void> {
     return firebase.auth().signOut();
   }
 }
