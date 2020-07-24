df453ba33b9b9b12f017a9e3dbd429a3e46f6f31
Thomas Kottke <t.kottke90@gmail.com>
2020-07-24 08:05:58 -0500

    feat: Add authentication endpoint to api

---
+++ project/functions/src/routes/auth.route.ts
@@ -0,0 +1,70 @@
+import BaseClass from '../classes/base-route.class';
+import Application from '../classes/application.class';
+import { IContext } from "../interfaces/routing.interfaces";
+
+import * as _ from 'lodash';
+
+const logError = async (context: IContext) => {
+
+  // console.dir(context);
+
+  return context;
+}
+
+class AuthRoute extends BaseClass {
+
+  constructor(app: Application) {
+    super(app, '/auth');
+
+    this.setup({
+      routes: [
+        { method: 'get', path: '/status', action: this.hasSession, errorHooks: [ logError ] },
+        { method: 'post', path: '/sessionLogin', action: this.getSession, errorHooks: [ logError ] }
+      ]
+    });
+  }
+
+  public async hasSession(context: IContext) {
+    const cookie = _.get(context, 'request.cookies.session', false);
+
+    return { status: !!cookie };
+  }
+
+  public async getSession(context: IContext) {
+    console.dir(context.data.tokenId);
+    const idToken = context.data.tokenId;
+    // const csrfToken = context.data.csrfToken;
+
+    // const csrfCookie = _.get(context, 'request.cookies.csrfToken', undefined);
+
+    // if (csrfToken !== csrfCookie) {
+    //   context.result._code = 401;
+    //   throw new Error('Unauthorized Request!');
+    // }
+
+    // Every Day
+    const expiresIn = 1000 * 60 * 60 * 24 * 3;
+
+    try {
+      const sessionCookie = await context.app.admin.auth().createSessionCookie(idToken, { expiresIn });
+      const cookieOptions = {
+        maxAge: expiresIn,
+        httpOnly: true,
+        secure: true
+      };
+
+      context.response.cookie('session', sessionCookie, cookieOptions);
+      return { status: 'success' };
+    } catch (err) {
+      console.error(err);
+      context.result = {
+        _code: 401
+      };
+      throw new Error('Unauthorized Request!');
+    }
+  }
+}
+
+exports.initialize = (app: Application) => {
+  return new AuthRoute(app);
+}
