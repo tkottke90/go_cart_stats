7f8684c3732128dc7d8c5a6e9ef66752a06e424c
Thomas Kottke <t.kottke90@gmail.com>
2020-07-24 07:52:58 -0500

    feat: init firebase before loading routes

--- project/functions/ui-src/bootstrap.ts
+++ project/functions/ui-src/bootstrap.ts
@@ -2,8 +2,17 @@ import './app/index';
 import { Router } from './app/router';
 import { routes } from './bootstrap.routes';
 
+import FirebaseService from './app/services/firebase.service';
+
 (async () => {
-  if ('serviceWorker' in navigator) {
+  try {
+    await FirebaseService.init();
+  } catch (err) {
+    console.dir(err);
+    return;
+  }
+
+  if ('serviceWorker' in navigator && false) {
     let refreshing = false;
     navigator.serviceWorker.addEventListener('controllerchange', () => {
         if (refreshing) { return; }
