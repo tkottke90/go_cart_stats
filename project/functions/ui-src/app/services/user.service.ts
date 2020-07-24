80db8312d04c5f75f2b885764c105f29ea777fb4
Thomas Kottke <t.kottke90@gmail.com>
2020-07-24 08:03:36 -0500

    feat: Add session generation call

--- project/functions/ui-src/app/services/user.service.ts
+++ project/functions/ui-src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import FirebaseService from './firebase.service';
-
+import HTTPService from './http.service';
 
 export default class UserService {
 
@@ -13,6 +13,23 @@ export default class UserService {
 
   public static loginWithGoogle() {
     return FirebaseService.signInWIthGoogle();
+  }
+
+  public static async checkSession() {
+    return HTTPService.get('/auth/status').toPromise();;
+  }
+
+  public static async getSession() {
+    const user = this.getUser();
+
+    if (!user) {
+      return;
+    }
+
+    const tokenId = await user.getIdToken();
+    const headers = new Headers();
+    headers.set('Content-Type', 'application/json');
+    return HTTPService.post('/auth/sessionLogin', { tokenId }, headers).toPromise();
   }
 
   public static logout() {
