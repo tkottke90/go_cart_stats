9e81e269f9a3b2d328f63b7bcc5202e7546c38e6
Thomas Kottke <t.kottke90@gmail.com>
2020-07-26 11:26:10 -0500

    feat: Add query string hook

---
+++ project/functions/src/hooks/query-string.hook.ts
@@ -0,0 +1,65 @@
+import { IContext } from '../interfaces/routing.interfaces';
+
+/*
+
+Examples:
+  ..?name=Thomas        => { name: "Thomas" }
+  ..?number[number]=19  => { number: { number: "19 " } }
+  ..?age[gt][number]=29 => { number: { gt: { number: "19 " } } }
+
+*/
+
+const convertParam = (type: string, value: string) => {
+  let output: string | boolean | number = '';
+  let retainStructure = true;
+  
+  switch(type) {
+    case 'number':
+      output = parseFloat(value);
+      retainStructure = false;
+    case 'boolean':
+      output = value === 'true';
+      retainStructure = false;
+    default:
+      output = value;
+  }
+
+  return { value: output, retainStructure };
+}
+
+const iterateOverObject = (key: string, param: any) => {
+  if (typeof param === 'object') {
+    const keys = Object.keys(param);
+
+    let output = { ...param };
+    keys.forEach( key => {
+      const { value, retainStructure } = iterateOverObject(key, param[key]);
+
+      if (retainStructure) {
+        output[key] = value;
+      } else {
+        output = value;
+      }
+    });
+
+    return { value: output, retainStructure: true };
+  }
+
+  return convertParam(key, param)  ;
+}
+
+export default function () {
+  return async (context: IContext) => {
+    const { value, } = iterateOverObject('', context.query);
+    context.query = value;
+
+    // console.dir(context.query);
+
+    // context.error = new Error('Test');
+    return context;
+  };
+}
+
+export {
+  iterateOverObject
+}
