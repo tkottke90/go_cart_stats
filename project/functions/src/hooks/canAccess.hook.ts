a7899b193ffd820c27c2697a1429248086e14267
Thomas Kottke <t.kottke90@gmail.com>
2020-08-09 14:42:57 -0500

    refactor(TK-15): Configure rbac hook to use constant roles

--- project/functions/src/hooks/canAccess.hook.ts
+++ project/functions/src/hooks/canAccess.hook.ts
@@ -1,20 +1,23 @@
 import { IContext } from '../interfaces/routing.interfaces';
-import * as jwt from 'jsonwebtoken';
 import * as _ from 'lodash';
 
 import { ROLES } from '../constants';
 
 export default function (level = ROLES.ADMIN) {
   return async (context: IContext) => {
-    const cookie = _.get(context, 'request.cookies.session', '');
-    const decodedToken = jwt.decode(cookie);
-
-    const userRole = _.get(decodedToken, 'customClaims', ROLES.USER);
-
-    if (userRole !== level) {
+    if (!context.user && level > ROLES.USER) {
       context.error = new Error('Forbidden');
       context.error._code = 403;
       return context;
+    }
+
+    console.dir(context.user);
+
+    const userRole = _.get(context.user, 'customClaims.role', ROLES.USER);
+
+    if (userRole > level) {
+      context.error = new Error('Forbidden');
+      context.error._code = 403;
     }
     
 
